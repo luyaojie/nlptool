4dc588e4e4e5b29bf55d25f1cc8b7d833a2a558d
Roger <luyaojie1993@gmail.com>
2017-08-21 22:20:13 +0800

    Bug fix in Segment_util

--- segment_utils.py
+++ segment_utils.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 import codecs
-from utils import write_str_to_out, any2utf8
+
+
+def any2utf8(text, errors='strict', encoding='utf8'):
+    """Convert a string (unicode or bytestring in `encoding`), to bytestring in utf8."""
+    if isinstance(text, unicode):
+        return text.encode('utf8')
+    # do bytestring -> unicode -> utf8 full circle, to ensure valid utf8
+    return unicode(text, encoding, errors=errors).encode('utf8')
+
+
+def write_str_to_out(out, write_str):
+    out.write(any2utf8(write_str))
 
 
 class ChineseWordSegmentor(object):
