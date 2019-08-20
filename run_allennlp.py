4b73f326577d2351908fffb7d5c2a5fccba9a6e6
Roger <luyaojie1993@gmail.com>
2019-08-20 16:34:19 +0800

    NLP Tool v0.1.5 run allennlp multi times

--- run_allennlp.py
+++ run_allennlp.py
@@ -2,24 +2,9 @@
 # -*- coding:utf-8 -*- 
 # Created by Roger on 2019-08-20
 import argparse
-import math
 import os
 import random as py_random
 import subprocess
-from collections import OrderedDict
-
-
-def load_config_from_file(filename):
-    cmd_option = OrderedDict()
-    with open(filename) as fin:
-        base_cmd = fin.readline().strip()
-        log_folder = fin.readline().strip()
-        if not os.path.exists(log_folder):
-            os.makedirs(log_folder)
-        for line in fin:
-            att = line.strip().split('\t')
-            cmd_option[att[0].strip()] = [value.strip() for value in att[1:]]
-    return base_cmd, cmd_option, log_folder
 
 
 def get_exp_id():
@@ -29,49 +14,6 @@ def get_exp_id():
     for i in range(4):
         exp_str += py_random.choice(alpha)
     return exp_str
-
-
-def cmd_generator(base_cmd, cmd_option, log_folder, pool_size=2, random=True, max_num=10, enum_time=1):
-    """
-
-    :param base_cmd:    Base Command
-    :param cmd_option:  {cmd -> [opt1, opt2, ..., optn]}
-    :param log_folder:  Log Folder Name
-    :param pool_size:
-    :param random:      random search ot not
-    :param max_num:     max exp number
-    :param enum_time:   max enum times
-    :return:
-    """
-    cmd_candidate_list = list()
-    if random:
-        for i in range(max_num):
-            cmd_candidate = list()
-            for cmd in cmd_option:
-                cmd_candidate += [cmd, py_random.choice(cmd_option[cmd])]
-            cmd_candidate_list += [cmd_candidate]
-    else:
-        cmd_list = []
-        for cmd in cmd_option:
-            cmd_list += [[]]
-            for option in cmd_option[cmd]:
-                cmd_list[-1] += [[cmd, option]]
-        for cmds in cmd_list:
-            if len(cmd_candidate_list) == 0:
-                cmd_candidate_list = cmds
-            else:
-                cmd_candidate_list = [c1 + c2 for c1 in cmds for c2 in cmd_candidate_list]
-        cmd_candidate_list *= enum_time
-
-    cmd_candidate_list = [("%s %s " % (base_cmd, ' '.join(cmd_candidate)),  # Command
-                           get_log_filename(log_folder, cmd_candidate),  # STDOUT
-                           )
-                          for cmd_candidate in cmd_candidate_list]
-
-    num_batch = int(math.ceil(len(cmd_candidate_list) / float(pool_size)))
-    for i in range(num_batch):
-        start, end = i * pool_size, (i + 1) * pool_size
-        yield cmd_candidate_list[start: end]
 
 
 def run_command(command):
